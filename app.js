e33b783f0a23fdaf54c70defdd4a70ccd2e7bd44
Senthamiz Kumaran <senthamizh.kumaran@gmail.com>
2020-01-25 08:47:05 +0530

    mongoose and user router

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const login = require('./lib/router/login');
+const users = require('./lib/router/users');
 const bodyParser = require('body-parser');
 const configs = require('./configs/configs');
+const mongoose = require('mongoose');
 
 class Server {
     constructor() {
@@ -12,12 +14,25 @@ class Server {
     appConfig() {
         this.app.use(bodyParser.json());
         this.app.use('/sso', login);
+        this.app.use('/users', users);
+    }
+
+    connectDB() {
+        mongoose.connect(configs.mongodb.connection, configs.mongodb.options, (err)=> {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('DB Connected')
+            }
+        })
     }
 
     startServer() {
+        this.appConfig();
         this.app.listen(this.port, (err)=>{
             console.log('Server has started on port',this.port)
         })
+        this.connectDB();
     }
 }
 
--- configs/configs.js
+++ configs/configs.js
@@ -2,7 +2,7 @@ module.exports = {
     jwtSecret: 'hereisthesecret',
     port: 3300,
     mongodb: {
-        connection: 'mongodb://admin:admin123@hurly-burly-dvff6.mongodb.net/test',
+        connection: 'mongodb+srv://hbdba:hbdba123@hurly-burly-dvff6.mongodb.net/authentication_db?retryWrites=true&w=majority',
         options: {
             useUnifiedTopology: true,
             useNewUrlParser: true
