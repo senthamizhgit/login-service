1bc87f0b6b23fcf37f1dc019e6966fb879240e50
Senthamiz Kumaran <>
2020-01-28 15:33:22 +0530

    rabbit mq changes

--- init-message-producer.js
+++ init-message-producer.js
@@ -2,7 +2,5 @@ const MessageProducer = require('./lib/worker/message-producer');
 
 const mp = new MessageProducer();
 
-(async function(){
-await mp.start();
-await mp.sendMessage('logs', 'this is my first log');
-})();
+
+mp.sendMessage('logs.exchange', 'hello');
