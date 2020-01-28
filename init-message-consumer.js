3efc4420f4ef6220d82fbce78cbbac09075f4007
Senthamiz Kumaran <>
2020-01-28 12:58:22 +0530

    rabbit mq changes

--- init-message-consumer.js
+++ init-message-consumer.js
@@ -2,7 +2,4 @@ const MessageConsumer = require('./lib/worker/message-consumer');
 
 const mc = new MessageConsumer();
 
-(async function(){
-await mc.start();
-await mc.consumeMessage('logs')
-})();
+mc.start();
